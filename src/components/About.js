51b9e0cb1ead2827c3a06eb53221b2b2c6c8c39b
el-sam <samurai.h@yahoo.com>
2026-02-08 09:11:40 +0800

    add function to component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { image } from "../data/data";
+import {name, city} from "../data/data";
+
+function showName (data) {
+  return (
+    <div>
+      <h3>the name we found in database is: {data}, is it you? </h3>
+    </div>
+  )
+};
+
+function verifyCity(input) {
+  const op = input == city;
+
+  return (
+    <div>
+      <p> we realise: {op ? `you are from ${input}` : "sorry, data not matching" } </p>
+    </div>
+  )
+};
+
 
 function About() {
   return (
     
     <div id="about">
       <h2>About Me</h2>
-      <p>some thing coming....</p> 
+      <section>{showName(name)}</section>
+      {/* <p>some thing coming....</p>  */}
+      <section> {verifyCity("London")}</section>
+      <section> {verifyCity("New York")}</section>
+      <section> {verifyCity("NewYork")}</section>
       <img src={image} alt="I made this"></img>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
 function Home() {
   // update the JSX being returned!
   return (
